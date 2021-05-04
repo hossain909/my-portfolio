1a13e55bb0a722bddc5885940595ae01419a1f5d
hossain909 <70032159+hossain909@users.noreply.github.com>
2021-05-04 22:24:35 +0600

    All Done

--- src/components/Home/About/About.js
+++ src/components/Home/About/About.js
@@ -16,7 +16,7 @@ const About = () => {
           </div>
           <div className="col-md-6 mt-3 align-self-center p-4 rounded">
             <div className=" text-light p-3 ">
-              <p><span className="d-block font-30 primaryColor bold-600">Hi,</span> <span className="primaryColor">I'm Taieb Hossain, </span> a Frontend Developer. My core skill is based on JavaScript and React Js and I love to do most of the things using JavaScript. I love to make the websites more open to the world. I am able to do any programming-related task by handling difficult and hard situations at any time. And have a lot of skills in JavaScript, React and Node.js And I have done many projects related to this.</p>
+              <p className="mb-3"><span className="d-block font-30 primaryColor bold-600">Hi,</span> <span className="primaryColor">I'm Taieb Hossain, </span> a Frontend Developer. My core skill is based on JavaScript and React Js and I love to do most of the things using JavaScript. I love to make the websites more open to the world. I am able to do any programming-related task by handling difficult and hard situations at any time. And have a lot of skills in JavaScript, React and Node.js And I have done many projects related to this.</p>
               <Buttons link="https://drive.google.com/file/d/1CcDw9qsrsjlw2l37Qe1-K4v3tL8-dVYb/view?usp=sharing" label="Download Resume" inverse></Buttons>
             </div>
           </div>
